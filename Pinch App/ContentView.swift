771a636e73df08efcefd09225cc1bc224332e600
Harry Lopez <harry.lopez@overactive.com>
2022-04-05 20:33:55 -0500

    Initial Commit

---
+++ Pinch App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pinch App
+//
+//  Created by Harry Lopez on 5/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
